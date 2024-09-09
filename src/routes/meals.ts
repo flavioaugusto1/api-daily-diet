da979b74246f5ba280dc00643f1fde6688f823f5
Flávio <f.augustosdn@gmail.com>
2024-09-08 23:01:08 -0300

    feat: atualizar refeição

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -36,15 +36,19 @@ export async function createMealRoutes(app: FastifyInstance) {
         },
     )
 
-    app.get('/', { preHandler: [checkSessionIdExists] }, async (request) => {
-        const { sessionId } = request.cookies
+    app.get(
+        '/',
+        { preHandler: [checkSessionIdExists] },
+        async (request, response) => {
+            const { sessionId } = request.cookies
 
-        const meals = await knex('meals')
-            .where('session_id', sessionId)
-            .select()
+            const meals = await knex('meals')
+                .where('session_id', sessionId)
+                .select()
 
-        return { meals }
-    })
+            return response.send({ meals })
+        },
+    )
 
     app.get(
         '/:id',
@@ -70,6 +74,72 @@ export async function createMealRoutes(app: FastifyInstance) {
             }
 
             return { meal }
+        },
+    )
+
+    app.put(
+        '/:id',
+        { preHandler: [checkSessionIdExists] },
+        async (request, response) => {
+            const idMealSchema = z.object({
+                id: z.string().uuid(),
+            })
+
+            const { id } = idMealSchema.parse(request.params)
+            const { sessionId } = request.cookies
+
+            const verifyIfMealExists = await knex('meals')
+                .where('id', id)
+                .andWhere('session_id', sessionId)
+
+            if (!verifyIfMealExists) {
+                return response.status(404).send({
+                    error: 'Refeição não localizada.',
+                })
+            }
+
+            const upadateMealsSchema = z.object({
+                name: z.string().optional(),
+                description: z.string().optional(),
+                in_diet: z.boolean().optional(),
+                date: z
+                    .string()
+                    .regex(
+                        new RegExp(
+                            /^(?:0[1-9]|[12]\d|3[01])\/(?:0[1-9]|1[0-2])\/\d{4}$/,
+                        ),
+                    )
+                    .optional(),
+                hour: z
+                    .string()
+                    .regex(new RegExp(/^(?:[01]\d|2[0-3]):[0-5]\d$/))
+                    .optional(),
+            })
+
+            const validateMeal = upadateMealsSchema.safeParse(request.body)
+
+            if (!validateMeal.success) {
+                return response.status(404).send({
+                    error: 'Um ou mais campos informados está inválidos.',
+                })
+            }
+
+            const { name, description, in_diet, date, hour } = validateMeal.data
+
+            await knex('meals')
+                .update({
+                    name: name,
+                    description: description,
+                    in_diet: in_diet,
+                    date_meal: date,
+                    hour_meal: hour,
+                })
+                .where('id', id)
+                .andWhere('session_id', sessionId)
+
+            response.send({
+                message: 'Atualizado com sucesso.',
+            })
         },
     )
 
